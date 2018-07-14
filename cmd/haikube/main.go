4297fe8f512f8d0a6fc084d11acb57f059bdb41f
John Calabrese <xchapter7x@gmail.com>
2018-07-14 09:41:10 -0400

    configurable source dir and path change

--- cmd/haikube/main.go
+++ cmd/haikube/main.go
@@ -13,7 +13,8 @@ import (
 
 var (
 	build       = kingpin.Command("build", "Build a container image from a buildpack and your code")
-	buildConfig = build.Flag("file", "Build config file path").Short('f').String()
+	buildConfig = build.Flag("config", "Build config file path").Short('c').String()
+	sourceDir   = build.Flag("source", "path to your code").Short('s').String()
 	push        = kingpin.Command("push", "Push your image to dockerhub.")
 	deploy      = kingpin.Command("deploy", "Deploy your application container to kubernetes.")
 	make        = kingpin.Command("make", "Build Push and Deploy your code")
@@ -33,15 +34,36 @@ func main() {
 		if err != nil {
 			log.Panicf("file read failed %v", err)
 		}
+
 		cfg.Parse(f)
-		dockerfileReader, cleanup, err := docker.CreateDockerfile(cfg.Buildpack, cfg.BaseImage, fmt.Sprintf("%v", cfg.Ports[0]), ".", docker.URIDownloader)
+		sourcePath, err := filepath.Abs(*sourceDir)
+		if err != nil {
+			log.Panicf("absolute path to your code not found: %v", err)
+		}
+
+		err = os.Chdir(sourcePath)
+		if err != nil {
+			log.Panicf("chdir failed: %v", err)
+		}
+
+		dockerfileReader, cleanup, err := docker.CreateDockerfile(
+			cfg.Buildpack,
+			cfg.BaseImage,
+			fmt.Sprintf("%v", cfg.Ports[0]),
+			".",
+			docker.URIDownloader,
+		)
 		defer cleanup()
 		if err != nil {
 			log.Panicf("create dockerfile failed: %v", err)
 		}
 
-		if err := docker.BuildImage(dockerfileReader, fmt.Sprintf("%s:%s", cfg.Image, cfg.Tag)); err != nil {
-			log.Panic(err)
+		err = docker.BuildImage(
+			dockerfileReader,
+			fmt.Sprintf("%s:%s", cfg.Image, cfg.Tag),
+		)
+		if err != nil {
+			log.Panic("build image fialed: ", err)
 		}
 	}
 }
